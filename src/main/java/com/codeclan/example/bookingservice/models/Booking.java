8ea0a38a3b9375372c87babde58ae004a1dca514
peaky76 <robertjamespeacock@gmail.com>
2020-09-10 10:54:13 +0100

    initial commit

---
+++ src/main/java/com/codeclan/example/bookingservice/models/Booking.java
@@ -0,0 +1,49 @@
+package com.codeclan.example.bookingservice.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="bookings")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String date; // Format: "dd-mm-yy"
+
+    @JsonIgnoreProperties({"bookings"})
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @JsonIgnoreProperties({"bookings"})
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    public Booking(String date, Course course, Customer customer) {
+        this.date = date;
+        this.course = course;
+        this.customer = customer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
---
+++ src/main/java/com/codeclan/example/bookingservice/models/Course.java
@@ -0,0 +1,75 @@
+package com.codeclan.example.bookingservice.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String town;
+
+    @Column(name="star_rating")
+    private int starRating;
+
+    @JsonBackReference
+    @OneToMany(mappedBy = "course")
+    @Cascade(org.hibernate.annotations.CascadeType.DELETE)
+    private List<Booking> bookings;
+
+    public Course(String name, String town, int starRating) {
+        this.name = name;
+        this.town = town;
+        this.starRating = starRating;
+        this.bookings = new ArrayList<Booking>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getStarRating() {
+        return starRating;
+    }
+
+    public void setStarRating(int starRating) {
+        this.starRating = starRating;
+    }
+
+    public void addBooking(Booking booking) {
+        this.bookings.add(booking);
+    }
+}
